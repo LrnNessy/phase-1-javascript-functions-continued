9ca69d44d4607e28911177c9508fb7ae11577d20
Lauren Shaughnessy <lrn.nessy@gmail.com>
2022-12-30 16:46:31 -0500

    commit tree 00ba834e9e1bd0197effefae17830bca25aeb98f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+  }
+  saturdayFun();
+  saturdayFun("bathe my dog");
+  
+const mondayWork = function(something = "go to the office") {
+    console.log(`This Monday, I will ${something}.`);
+    return `This Monday, I will ${something}.`;
+  }; 
+  mondayWork();
+  mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "a hard worker") {
+       console.log(`You are ${flair}${adjective}${flair}!`);
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+   
+  }
+  wrapAdjective()();
+  wrapAdjective("#")("a something");
